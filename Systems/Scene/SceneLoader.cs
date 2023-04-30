7d6ce1c512985236f93e927d3f316110a59896ed
HR-Hello-World <1594679224@qq.com>
2023-04-30 21:56:59 +0800

    HR‘s annotation v7.2

--- Systems/Scene/SceneLoader.cs
+++ Systems/Scene/SceneLoader.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
-
 using AutumnFramework;
 
+
+//自定义VGF.SceneSystem，实现场景加载器的功能
 namespace VGF.SceneSystem
 {
     [Beans]
@@ -27,16 +28,20 @@ namespace VGF.SceneSystem
         private Dictionary<string, Action<Msg>> SceneEvent = new();
 
         public Action AfterSceneLoaded;
+        
+        //开局调用
         private void Start()
         {
             this.Bean(); //由于某种复杂原因，需要加这个。详细直接找zmq本人
         }
 
+        //根据场景名称加载指定场景
         public void SwitchSceneByName(string name)
         {
             StartCoroutine(LoadSceneSetActive(name));
         }
 
+        //加载场景并激活的协程函数
         private IEnumerator LoadSceneSetActive(string name)
         {
             for (int i = 0; i < SceneManager.sceneCount; i++)
@@ -48,8 +53,11 @@ namespace VGF.SceneSystem
             bool isLoadingEnd = false;
 
             async = SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
-            async.allowSceneActivation = false;  //属性设置为 false，这意味着新场景将不会在加载完成后自动激活。这样做的原因是为了在新场景完全加载之前显示进度条或者执行其他逻辑，等到加载完成后再激活新场景。
+            //属性设置为 false，这意味着新场景将不会在加载完成后自动激活。这样做的原因是为了在新场景完全加载之前显示进度条或者执行其他逻辑，等到加载完成后再激活新场景。
+            async.allowSceneActivation = false;
             TransitionCanvas.enabled = true;
+
+            //更新场景加载进度条的值
             while (!async.isDone)
             {
                 if (async.progress < 0.9f)
@@ -84,11 +92,14 @@ namespace VGF.SceneSystem
             yield break;
         }
 
+        //用于传递事件需要的参数
         public struct Msg
         {
             Scene OriginScene;
 
         }
+
+        //将一个场景名字和事件绑定，以在场景加载完毕后执行该事件
         public void BindSceneEvent(string sceneName, Action<Msg> action)
         {
             if (SceneEvent.TryAdd(sceneName, action))
@@ -104,15 +115,20 @@ namespace VGF.SceneSystem
                 //throw new Exception("SceneLoader未知错误，请检查");
             }
         }
+
+        //对象被启用时会调用
         private void OnEnable()
         {
             SceneManager.sceneLoaded += OnSceneLoaded;
         }
 
+        //对象被禁用时会调用
         private void OnDisable()
         {
             SceneManager.sceneLoaded -= OnSceneLoaded;
         }
+
+        //场景加载完毕后的回调函数
         void OnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
             if (SceneEvent.TryGetValue(scene.name, out Action<Msg> action))
